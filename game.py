fe500d2bdfff2c7292afe7188377715cd8aa32fe
Milan <mujdar.milan@gmail.com>
2024-03-13 01:08:08 +0200

    sketch for menu

--- game.py
+++ game.py
@@ -2,17 +2,20 @@ import pygame
 from constants import *
 from squares import Squares
 from chess import Chess
-
+from menu import GameMenu
 
 class GameController:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Chess")
-        self.chessScreen = pygame.display.set_mode(SCREENSIZE)
+        self.display = pygame.display.set_mode(SCREENSIZE)
+        self.chessScreen = pygame.Surface(SCREENSIZE)
+        self.menuScreen = pygame.Surface(SCREENSIZE)
         # self.gameScreen = pygame.display.set_mode(GAMESCREENSIZE)
         self.clock = pygame.time.Clock()
         self.holdingClick = False
         self.mouse_pos = None
+        self.gameState = MENU
 
     def startGame(self):
         self.chessGame = Chess()
@@ -27,28 +30,40 @@ class GameController:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    self.mouse_pos = event.pos
-                    self.holdingClick = True
-                    self.chessGame.handleClick(self.mouse_pos, LEFT_CLICK_PRESS)
-                if event.button == 3:
-                    self.mouse_pos = event.pos
-                    self.chessGame.unselectEverything()
-                    self.holdingClick = False
-                    self.chessGame.handleClick(self.mouse_pos, RIGHT_CLICK)
-            if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    self.mouse_pos = event.pos
-                    self.holdingClick = False
-                    self.chessGame.handleClick(self.mouse_pos, LEFT_CLICK_RELEASE)
+
+            if self.gameState == PLAYING_CASUAL:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.mouse_pos = event.pos
+                        self.holdingClick = True
+                        self.chessGame.handleClick(self.mouse_pos, LEFT_CLICK_PRESS)
+                    if event.button == 3:
+                        self.mouse_pos = event.pos
+                        self.chessGame.unselectEverything()
+                        self.holdingClick = False
+                        self.chessGame.handleClick(self.mouse_pos, RIGHT_CLICK)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        self.mouse_pos = event.pos
+                        self.holdingClick = False
+                        self.chessGame.handleClick(self.mouse_pos, LEFT_CLICK_RELEASE)
+
+            if self.gameState == MENU:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.gameState = PLAYING_CASUAL
 
         # print(self.holdingClick)
-        self.chessGame.holdingClick(self.mouse_pos, self.holdingClick)
+        if self.gameState == PLAYING_CASUAL:
+            self.chessGame.holdingClick(self.mouse_pos, self.holdingClick)
 
     def render(self):
-        self.chessGame.render(self.chessScreen)
-        # self.gameScreen.fill((0, 0, 0))
-        # chess_rect = self.chessScreen.get_rect(center=self.gameScreen.get_rect().center)
-        # self.gameScreen.blit(self.chessScreen, self.chess_rect)
+
+        if self.gameState == PLAYING_CASUAL:
+            self.chessGame.render(self.chessScreen)
+            self.display.blit(self.chessScreen, (0,0))
+
+        if self.gameState == MENU:
+            self.menuScreen.fill((50, 50, 50))
+            self.display.blit(self.menuScreen, (0,0))
+
         pygame.display.update()
---
+++ menu.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import *
+
+class GameMenu:
+    def __init__(self):
+        pass
+
+    def handleEvents(self, event):
+        pass
+
+    def render(self, screen):
+        pass
